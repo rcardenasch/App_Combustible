4b54f72348cf58858b073a6a7a19a37cd8a67e9e
ROMEL CARDENAS CHAUCA <rcardenasc@juntos.gob.pe>
2026-06-25 14:39:33 -0500

    Optimiza consulas BD y crea permisos para mod reportes

--- config.py
+++ config.py
@@ -15,15 +15,20 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = 'super-secret-key'
 
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "postgresql+psycopg2://postgres:123456@localhost:5433/App_Combustible"
-    )
-
+    # Corrección para Render/Heroku: postgres:// a postgresql://
+    uri = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:123456@localhost:5433/App_Combustible")
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     UPLOAD_FOLDER = os.path.join(BASE_DIR, "static/uploads")
 
+    # 🔥 OPTIMIZACIÓN EXCLUSIVA PARA NEON TECH
     SQLALCHEMY_ENGINE_OPTIONS = {
-    "pool_pre_ping": True,
-}
+        "pool_size": 4,             # Neon gratis permite pocas conexiones concurrentes. 4 es ideal.
+        "max_overflow": 2,          # Permite superar el límite brevemente si hay picos.
+        "pool_recycle": 280,        # Neon cierra conexiones inactivas a los 5 min (300s). Las reciclamos a los 4.6 min.
+        "pool_timeout": 15,         # Tiempo máximo de espera por una conexión libre antes de dar timeout.
+        "pool_pre_ping": True,      # Verifica si la conexión sigue viva antes de lanzar el query.
+    }
